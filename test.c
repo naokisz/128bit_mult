c06c74b3098b93c772cc63005c9c04509295653a
naokisz <naokihirokitomoki@gmail.com>
2017-07-06 02:03:34 +0900

    do whileをforに修正。

--- test.c
+++ test.c
@@ -16,22 +16,26 @@ int main() {
 	int bit;
 	uint64_t a = 0, b = 0, lo_asm, hi_asm, lo, hi;
 
-	do {
-		printf("a=%"PRIu64"\n", a);
-		do {
+	for(;;) {
+		for(;;) {
+			printf("a=%"PRIu64"\t", a);
+			printf("b=%"PRIu64"\n", b);
 			mul128_asm_x86_64(a, b, &lo_asm, &hi_asm);
 			mult_128bit(a, b, &lo, &hi);
 			if(lo_asm != lo || hi_asm != hi) {
 				printf("Not much!!!!!!!!\n");
 				printf("a=%"PRIu64"\nb=%"PRIu64"\n", a, b);
-
+	
 				printf("hi=%"PRIX64"\nlo=%"PRIX64"\n", hi, lo);
 				printf("hi_asm=%"PRIX64"\nlo_asm=%"PRIX64"\n", hi_asm, lo_asm);
+				return -1;
 			}
-			b++;
-		} while(b < UINT64_MAX);
-		a++;
-	} while(a < UINT64_MAX);
+			if(b == UINT64_MAX) break;
+			else b++;
+		}
+		if(a == UINT64_MAX) break;
+		else a++;
+	}
 
 	return 0;
 }
